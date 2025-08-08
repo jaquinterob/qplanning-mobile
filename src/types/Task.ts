5c27d7c7ace7cbeacede8d36d2006d8393d8ac02
John A. Quintero <jquintero@migrante.com>
2025-08-08 00:06:34 -0500

    mpv

--- src/types/Task.ts
+++ src/types/Task.ts
@@ -12,4 +12,10 @@ export interface Task extends Activity {
   emoji?: string;
   assignedTo?: string;
   estimatedCompletionTime?: number; // in minutes
+  timerStarted?: boolean;
+  timerStartTime?: number; // timestamp
+  timerPaused?: boolean;
+  elapsedTime?: number; // milliseconds elapsed when paused
+  isOverdue?: boolean;
+  actualCompletionTime?: number; // tiempo real que tomó completar la tarea (en milisegundos)
 }
